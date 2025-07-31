8c1154d1475451cc7b59351ed60b2001d26b4e17
Martin Olausson <martin.olausson@bahnhof.se>
2025-07-31 15:28:48 +0200

    fix: make TestAutoPause compatible with Fyne v2.6
    Ensure Fyne's event loop is initialized in TestAutoPause by using
    test.NewApp(). This allows UI-bound state changes to propagate
    correctly during tests, fixing failures after upgrading to Fyne v2.6.

--- test/punchclockcontroller_test.go
+++ test/punchclockcontroller_test.go
@@ -2,78 +2,103 @@ package test
 
 import (
 	"testing"
+	"time"
+
+	"fyne.io/fyne/v2/test"
 
 	mocks "github.com/simmarn/punchclock/mocks/github.com/simmarn/punchclock/pkg"
 	punchclock "github.com/simmarn/punchclock/pkg"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-/*
-	func TestAutoPause(t *testing.T) {
-		assert := assert.New(t)
-
-		mockRs := MockRecordStorage(t)
-		mockPrefs := mocks.NewPreferencesWrapper(t)
-		mockPrefs.EXPECT().SetString(mock.Anything, mock.Anything)
-		mockPrefs.EXPECT().SetBool(mock.Anything, mock.Anything)
-		mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false).Once()
-		controller := punchclock.NewPunchclockController(mockRs, mockPrefs, nil)
-		mockPrefs.AssertCalled(t, "GetBool", punchclock.PREFAUTOPAUSEACTIVE)
-		assert.Equal(punchclock.WORKING, GetStatus(controller))
-
-		start := time.Now().Format(punchclock.HHMMSS24h)
-		end := time.Now().Add(time.Minute).Format(punchclock.HHMMSS24h)
-		mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSESTART).Return(start)
-		mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSEEND).Return(end)
-		err := controller.SetAutoPauseInterval(start, end)
-		mockPrefs.AssertCalled(t, "SetString", punchclock.PREFAUTOPAUSESTART, start)
-		mockPrefs.AssertCalled(t, "SetString", punchclock.PREFAUTOPAUSEEND, end)
-		mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
-		assert.Nil(err)
-		assert.Equal(punchclock.WORKING, GetStatus(controller))
-
-		mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(true).Once()
-		err = controller.SetAutoPause(true)
-		assert.Nil(err)
-		assert.Equal(punchclock.PAUSED, GetStatus(controller))
-
-		mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false).Once()
-		controller.SetAutoPause(false)
-		assert.Equal(punchclock.WORKING, GetStatus(controller))
-
-		mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(true).Once()
-		controller.SetAutoPause(true)
-		assert.Equal(punchclock.PAUSED, GetStatus(controller))
-
-		err = controller.SetAutoPauseInterval("11:00", "11:00")
-		assert.NotNil(err)
-		mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
-
-		err = controller.SetAutoPauseInterval("11:00", "12,00")
-		assert.NotNil(err)
-		mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
-
-		err = controller.SetAutoPauseInterval("eleven", "12:00")
-		assert.NotNil(err)
-		mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
+func waitForStatus(c *punchclock.PunchclockController, expected string, t *testing.T) {
+	deadline := time.Now().Add(500 * time.Millisecond)
+	for time.Now().Before(deadline) {
+		status, _ := c.Status.Get()
+		if status == expected {
+			return
+		}
+		time.Sleep(10 * time.Millisecond)
 	}
-
-	func TestAutoPauseNotSet(t *testing.T) {
-		assert := assert.New(t)
-
-		mockPrefs := mocks.NewPreferencesWrapper(t)
-		mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false)
-		mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSESTART).Return("")
-		mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSEEND).Return("")
-
-		mockRs := MockRecordStorage(t)
-		controller := punchclock.NewPunchclockController(mockRs, mockPrefs, nil)
-		assert.Equal(punchclock.WORKING, GetStatus(controller))
-
-		err := controller.SetAutoPause(true)
-		assert.NotNil(err)
+	status, _ := c.Status.Get()
+	if status != expected {
+		t.Fatalf("expected status %s, got %s", expected, status)
 	}
-*/
+}
+
+func TestAutoPause(t *testing.T) {
+	app := test.NewApp()
+	defer app.Quit()
+	assert := assert.New(t)
+
+	mockRs := MockRecordStorage(t)
+	mockPrefs := mocks.NewPreferencesWrapper(t)
+	mockPrefs.EXPECT().SetString(mock.Anything, mock.Anything)
+	mockPrefs.EXPECT().SetBool(mock.Anything, mock.Anything)
+	mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false).Once()
+	controller := punchclock.NewPunchclockController(mockRs, mockPrefs, nil)
+	mockPrefs.AssertCalled(t, "GetBool", punchclock.PREFAUTOPAUSEACTIVE)
+	waitForStatus(controller, punchclock.WORKING, t)
+	assert.Equal(punchclock.WORKING, GetStatus(controller))
+
+	start := time.Now().Format(punchclock.HHMMSS24h)
+	end := time.Now().Add(time.Minute).Format(punchclock.HHMMSS24h)
+	mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSESTART).Return(start)
+	mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSEEND).Return(end)
+	err := controller.SetAutoPauseInterval(start, end)
+	mockPrefs.AssertCalled(t, "SetString", punchclock.PREFAUTOPAUSESTART, start)
+	mockPrefs.AssertCalled(t, "SetString", punchclock.PREFAUTOPAUSEEND, end)
+	mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
+	assert.Nil(err)
+	waitForStatus(controller, punchclock.WORKING, t)
+	assert.Equal(punchclock.WORKING, GetStatus(controller))
+
+	mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(true).Once()
+	err = controller.SetAutoPause(true)
+	assert.Nil(err)
+	waitForStatus(controller, punchclock.PAUSED, t)
+	assert.Equal(punchclock.PAUSED, GetStatus(controller))
+
+	mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false).Once()
+	controller.SetAutoPause(false)
+	waitForStatus(controller, punchclock.WORKING, t)
+	assert.Equal(punchclock.WORKING, GetStatus(controller))
+
+	mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(true).Once()
+	controller.SetAutoPause(true)
+	waitForStatus(controller, punchclock.PAUSED, t)
+	assert.Equal(punchclock.PAUSED, GetStatus(controller))
+
+	err = controller.SetAutoPauseInterval("11:00", "11:00")
+	assert.NotNil(err)
+	mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
+
+	err = controller.SetAutoPauseInterval("11:00", "12,00")
+	assert.NotNil(err)
+	mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
+
+	err = controller.SetAutoPauseInterval("eleven", "12:00")
+	assert.NotNil(err)
+	mockPrefs.AssertNumberOfCalls(t, "SetString", 2)
+}
+
+func TestAutoPauseNotSet(t *testing.T) {
+	assert := assert.New(t)
+
+	mockPrefs := mocks.NewPreferencesWrapper(t)
+	mockPrefs.EXPECT().GetBool(punchclock.PREFAUTOPAUSEACTIVE).Return(false)
+	mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSESTART).Return("")
+	mockPrefs.EXPECT().GetString(punchclock.PREFAUTOPAUSEEND).Return("")
+
+	mockRs := MockRecordStorage(t)
+	controller := punchclock.NewPunchclockController(mockRs, mockPrefs, nil)
+	assert.Equal(punchclock.WORKING, GetStatus(controller))
+
+	err := controller.SetAutoPause(true)
+	assert.NotNil(err)
+}
+
 func MockRecordStorage(t *testing.T) punchclock.RecordStorage {
 	mockRs := mocks.NewRecordStorage(t)
 	mockRs.EXPECT().Load().Return(CreateRecordsForTest(), nil)
